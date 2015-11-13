952a8fde42b730bf52661d9b6c4feaf780f7af9e
danielkk1c <danielkk1c@gmail.com>
2015-11-13 15:49:48 +0200

    q1

--- src/danielc/tar3Servlet.java
+++ src/danielc/tar3Servlet.java
@@ -3,12 +3,16 @@ import java.io.IOException;
 
 import javax.servlet.http.*;
 
+
 @SuppressWarnings("serial")
 public class tar3Servlet extends HttpServlet {
 	int radius = 50;
-	double pie = 3.14;
+	double num = Math.PI;
+
 			public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 			resp.setContentType("text/plain");
-			resp.getWriter().println("Area of circle with radius: " + (radius) + " is: " + ((radius * radius)*pie) + " squarecm.");
+			resp.getWriter().println("Area of circle with radius: " + (radius) + " is: " + (int)((radius * radius)*num) + " squarecm.");
 	}
+	
+
 }
