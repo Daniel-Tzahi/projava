768730288980a2c491cd9aae1f6274635ca4a29f
tzahita <tamamtzahi@gmail.com>
2015-11-13 14:44:25 +0200

    jsa

--- src/danielc/tar3Servlet.java
+++ src/danielc/tar3Servlet.java
@@ -5,8 +5,10 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class tar3Servlet extends HttpServlet {
+	int radius = 50;
+	double pie = 3.14;
 			public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 			resp.setContentType("text/plain");
-			resp.getWriter().println("1 + 1 == " + (1+1));
+			resp.getWriter().println("Area of circle with radius: " + (radius) + " is: " + ((radius * radius)*pie) + " squarecm.");
 	}
 }
