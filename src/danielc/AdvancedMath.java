fb5dbbf4b92f49c9e8d92bd43b0662676b6606c5
tzahita <tamamtzahi@gmail.com>
2015-11-17 13:01:18 +0200

    v2

---
+++ src/danielc/AdvancedMath.java
@@ -0,0 +1,27 @@
+package danielc;
+
+public class AdvancedMath {
+	
+	int radius = 50;
+	double num = Math.PI;
+	int degree = 30;
+	double hypotenuse =50;
+	double sin = Math.sin(degree);
+	double opposite;
+	int base = 20;
+	int exp = 13;
+	double power =  Math.pow(base, exp);
+	
+	String line1= new String ("Area of circle with radius: " + (radius) + " is: " + (int)((radius * radius)*num) + " squarecm.");
+	String line2= new String ("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "  + (sin * hypotenuse)+" cm");
+	String line3= new String ("Power of 20 with exp of 13 is: " + power);
+	
+	String result= new String (line1 +"<br>" + line2 +"<br>" + line3 + "<br>");
+	
+
+	  public void setRelust(String result){
+	       this.result = result;
+	  }
+
+	
+}
--- src/danielc/tar3Servlet.java
+++ src/danielc/tar3Servlet.java
@@ -1,21 +1,21 @@
 package danielc;
 import java.io.IOException;
 
+import danielc.AdvancedMath;
+
 import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
+@SuppressWarnings({ "serial", "unused" })
 public class tar3Servlet extends HttpServlet {
-	
-			int radius = 50;
-			double num = Math.PI;
-			
-			String line1= new String ("Area of circle with radius: " + (radius) + " is: " + (int)((radius * radius)*num) + " squarecm.");
-			String line2= new String ();
-			String line3= new String ();
-			String result= new String (line1 +"<br>" + line2 +"<br>" + line3 + "<br>");
-			
+		String result;
+		
+	  public String getResult(){
+	       return this.result;
+	  }
 			public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-			resp.setContentType("text/plain");
+			resp.setContentType("text/html");
+
+			
 			resp.getWriter().println("Answer to Question 1:");
 			resp.getWriter().println(result);
 			
