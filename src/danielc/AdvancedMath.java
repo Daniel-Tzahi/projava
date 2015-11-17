7a5aeb6cdd99812227ebf17cb0988be4aa182db2
tzahita <tamamtzahi@gmail.com>
2015-11-17 16:25:10 +0200

    v3

--- src/danielc/AdvancedMath.java
+++ src/danielc/AdvancedMath.java
@@ -10,18 +10,19 @@ public class AdvancedMath {
 	double opposite;
 	int base = 20;
 	int exp = 13;
-	double power =  Math.pow(base, exp);
+	int power =  (int) Math.pow(base, exp);
 	
 	String line1= new String ("Area of circle with radius: " + (radius) + " is: " + (int)((radius * radius)*num) + " squarecm.");
-	String line2= new String ("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "  + (sin * hypotenuse)+" cm");
+	String line2= new String ("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "  + (sin*hypotenuse) +" cm");
 	String line3= new String ("Power of 20 with exp of 13 is: " + power);
 	
-	String result= new String (line1 +"<br>" + line2 +"<br>" + line3 + "<br>");
+	
 	
 
-	  public void setRelust(String result){
-	       this.result = result;
-	  }
+	public String getResults() {
+		String result= new String (line1 +"<br>" + line2 +"<br>" + line3 + "<br>");
+		return result;
+	}
 
 	
 }
--- src/danielc/tar3Servlet.java
+++ src/danielc/tar3Servlet.java
@@ -7,17 +7,14 @@ import javax.servlet.http.*;
 
 @SuppressWarnings({ "serial", "unused" })
 public class tar3Servlet extends HttpServlet {
-		String result;
-		
-	  public String getResult(){
-	       return this.result;
-	  }
+	
 			public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 			resp.setContentType("text/html");
 
-			
+			AdvancedMath mathManager = new AdvancedMath();
+			String resultStr = mathManager.getResults();
 			resp.getWriter().println("Answer to Question 1:");
-			resp.getWriter().println(result);
+			resp.getWriter().println(resultStr);
 			
 			
 	}
